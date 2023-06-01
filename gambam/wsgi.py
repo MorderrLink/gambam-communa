59a4bd9a376d05e11dd4a598ebc566030f775575
Alex <burmenskiy90@bk.ru>
2023-06-01 10:21:23 +0300

    Commit 14

--- gambam/wsgi.py
+++ gambam/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-import os
-import sys
+# import os
+# import sys
 # DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 # sys.path.append(DJANGO_PATH)
 
