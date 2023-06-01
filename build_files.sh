fa1c87d0aac46b27ec351166c840b84743979472
Alex <burmenskiy90@bk.ru>
2023-06-01 10:24:56 +0300

    Commit 15

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,6 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+pip3.9 install --disable-pip-version-check --target
+--upgrade -r /vercel/path0/requirements.txt
 echo " BUILD END"
